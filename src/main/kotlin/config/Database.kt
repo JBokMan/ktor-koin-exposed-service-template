129e3e59e99e14d1f0d2f0b049398669a10ace93
Julian Bokelmann <bokelmann@sipgate.de>
2025-02-21 13:00:14 +0100

    ✨ add postgres and remove h2

--- src/main/kotlin/config/Database.kt
+++ src/main/kotlin/config/Database.kt
@@ -1,21 +1,32 @@
 package com.example.config
 
 import com.example.repository.UserService.Users
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.github.oshai.kotlinlogging.KLogger
 import io.ktor.server.application.Application
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.koin.ktor.ext.inject
 
 fun Application.configureDatabase() {
-    val myDatabase = Database.connect(
-        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-        user = "root",
-        driver = "org.h2.Driver",
-        password = "",
-    )
 
-    transaction(myDatabase) {
+    val log by inject<KLogger>()
+
+    Database.connect(HikariDataSource(HikariConfig().apply {
+        jdbcUrl = "jdbc:postgresql://localhost:5432/mydatabase"
+        driverClassName = "org.postgresql.Driver"
+        username = "myuser"
+        password = "mypassword"
+        maximumPoolSize = 10
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+    }))
+
+    transaction {
         SchemaUtils.create(Users)
     }
-}
 
+    log.info { "Database initialized successfully" }
+}
--- src/main/kotlin/config/Koin.kt
+++ src/main/kotlin/config/Koin.kt
@@ -5,7 +5,6 @@ import com.example.repository.UserService
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.application.log
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 
