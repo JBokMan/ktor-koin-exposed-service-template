444b17b2c0a891919c5b5786084d9fa4f1e3e462
Julian Bokelmann <bokelmann@sipgate.de>
2025-02-28 17:31:32 +0100

    ✨ add rennovate auto-merge and basic github build and test workflow

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,8 +1,8 @@
 package com.example
 
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
+import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,9 +13,8 @@ class ApplicationTest {
         application {
             module()
         }
-        client.get("/").apply {
+        client.get("/users").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
     }
-
 }
