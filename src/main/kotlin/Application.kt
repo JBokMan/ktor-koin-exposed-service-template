f1aa40f79b24624a50e51bf72e28a95ccd973b4b
Julian Bokelmann <bokelmann@sipgate.de>
2025-03-31 08:52:13 +0200

    ✨ add kafka configuration and consumer

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,12 +2,14 @@ package com.example
 
 import com.example.boundary.UserController
 import com.example.config.CohortConfiguration
+import com.example.config.KafkaConfiguration
 import com.example.config.configureKoin
 import com.example.config.configureSerialization
 import com.example.config.configureSwagger
 import config.DatabaseConfiguration
 import io.ktor.server.application.Application
 import io.ktor.server.cio.CIO
+import io.ktor.server.config.yaml.YamlConfig
 import io.ktor.server.engine.embeddedServer
 import org.koin.ktor.ext.inject
 
@@ -16,12 +18,17 @@ fun main() {
 }
 
 fun Application.module() {
-    configureKoin()
+    val config = YamlConfig("application.yaml") ?: error("Failed to load configuration")
+
+    configureKoin(config)
     configureSerialization()
     configureSwagger()
 
     val databaseConfiguration: DatabaseConfiguration by inject()
     databaseConfiguration.configureDatabase()
+
+    val kafkaConfiguration: KafkaConfiguration by inject()
+    kafkaConfiguration.configureKafka()
 
     val cohortConfiguration: CohortConfiguration by inject()
     cohortConfiguration.configureCohort(this)
--- src/main/kotlin/config/Koin.kt
+++ src/main/kotlin/config/Koin.kt
@@ -2,9 +2,16 @@ package com.example.config
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.config.yaml.YamlConfig
 import org.koin.ksp.generated.defaultModule
 import org.koin.ktor.plugin.Koin
 
-fun Application.configureKoin() {
-    install(Koin) { modules(defaultModule, networkModule) }
+fun Application.configureKoin(config: YamlConfig) {
+    val ktorModule =
+        org.koin.dsl.module {
+            single<ApplicationConfig> { config }
+        } // Creates A Bean of ApplicationConfig
+
+    install(Koin) { modules(ktorModule, defaultModule, networkModule) }
 }
